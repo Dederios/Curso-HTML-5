10ae8a2eb1a1091a84eb542884eda24ab4341c2c
Daniel <dedezinhorios@gmail.com>
2020-01-28 15:24:32 -0300

    Finished

--- src/javascript/funcoes.js
+++ src/javascript/funcoes.js
@@ -1,13 +1,12 @@
-function mudaFoto (foto)
-{
+function mudaFoto(foto) {
     document.getElementById("icone").src = foto;
 }
 
 function listaCidades(estado) {
     var cidadesPorEstado = {
-        AC: ["Acrelândia","Assis Brasil","Tarauacá","Xapuri"],
-        AL: ["Anadia","Arapiraca","Atalaia","Barra", "Água Branca"],
-        AM: ["Alvarães","Amaturá","Anamã","Tefé","Tonantins","Uarini","Urucará","Urucurituba"],
+        AC: ["Acrelândia", "Assis Brasil", "Tarauacá", "Xapuri"],
+        AL: ["Anadia", "Arapiraca", "Atalaia", "Barra", "Água Branca"],
+        AM: ["Alvarães", "Amaturá", "Anamã", "Tefé", "Tonantins", "Uarini", "Urucará", "Urucurituba"],
         BA: ["Salvador", "Varzéa da Roça", "Mairi", "Camaçari", "Feira de Santana"],
         SP: ["São Paulo", "Caraicuíba", "Osasco", "Barueri", "Itapevi", "Cotia", "Jandira"],
 
@@ -15,13 +14,13 @@ function listaCidades(estado) {
     var options = '';
     if (cidadesPorEstado.hasOwnProperty(estado)) {
         for (var i = 0; i < cidadesPorEstado[estado].length; i++) {
-            options += '<option value="'+cidadesPorEstado[estado][i]+'"/>';
+            options += '<option value="' + cidadesPorEstado[estado][i] + '"/>';
         }
     }
     document.getElementById('cidades').innerHTML = options;
 }
 
-function calc_total(){
+function calc_total() {
     var qtd = parseInt(document.getElementById('cQtd').value);
     var tot = qtd * 1500;
     document.getElementById('cTot').value = tot;
